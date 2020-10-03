aa7f46ad912b5a176fd3492fe7e2fc4df845bdb7
limay <limay.amboka@gmail.com>
2020-10-03 17:01:31 +0100

    modif emplacement des back et front

--- controllers/Frontend.php
+++ controllers/Frontend.php
@@ -33,12 +33,7 @@ class Frontend{
       require('view/frontend/login.php');
      }
 
-
 /*************************************************=SECTION-POSTS-COMMENTS=***************************************/
-    public function createPostView(){
-      require('view/frontend/createPost.php');
-    }
-
     public function listPosts($page = null){/*--------------- LISTES-POSTS ----------------*/
       $this->postManager = new PostManager();
 
@@ -47,24 +42,7 @@ class Frontend{
       $posts = $this->postManager->getPosts($pagination->getItemPerPage(), $pagination->getStartIndex());
 
        require('view/frontend/listPostsView.php');
-  }
-  
-                                          /*--------------- VUE ECRIRE-POST ----------------*/
-    public function createPost(){ 
-    if(isset($_POST['titre']) && isset($_POST['content'])){    
-        $this->postManager = new PostManager();
-        $this->postManager->addPost($_POST['titre'],$_POST['content']);
-        header('Location:listPosts');
-    }
-  }
-
-    public function editPostView($id){  
-     if($this->getId !=1){                  /*--------------- VUE-EDIT-POST ----------------*/
-        $this->postManager = new PostManager();
-        $post = $this->postManager->getPost($this->getId);
-        require('view/frontend/newPostView.php');
-      }
-    }
+  }   
 
     /*--------------- MISE-A-JOUR-POST ----------------*/
     public function editPost(){ 
